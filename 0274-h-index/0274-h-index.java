0db195c9c09e4e878f2b478d4f70186efe4ec6a8
Anuj Thakur <82753410+anuj-thakur-513@users.noreply.github.com>
2023-05-19 15:42:35 +0530

    Time: 2 ms (59.25%), Space: 40.4 MB (80.58%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++) {
+            if(citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+
+        return 0;
+    }
+}
