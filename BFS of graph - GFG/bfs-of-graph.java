4afeea050aee6ac6d838d3813839a66ae18dbec3
Anuj Thakur <82753410+anuj-thakur-513@users.noreply.github.com>
2023-07-13 18:34:11 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.java
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -45,16 +45,17 @@ class Solution {
         Queue<Integer> q = new LinkedList<>();
         
         q.offer(0);
+        visited[0] = 1;
         
         while(!q.isEmpty()) {
             int curr = q.poll();
+            bfs.add(curr);
             
-            if(visited[curr] != 1) {
-                visited[curr] = 1;
-                bfs.add(curr);
-                for(int i = 0; i < adj.get(curr).size(); i++) {
-                    q.offer(adj.get(curr).get(i));
-                }   
+            for(int it: adj.get(curr)) {
+                if(visited[it] != 1) {
+                    visited[it] = 1;
+                    q.offer(it);
+                }
             }
         }
         
