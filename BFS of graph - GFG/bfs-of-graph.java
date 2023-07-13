e618c31d471910ae2d3197f8a3f58c60736b6ffc
Anuj Thakur <82753410+anuj-thakur-513@users.noreply.github.com>
2023-07-13 18:32:04 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
+        if(v == 0) {
+            return new ArrayList<Integer>();
+        }
+        
+        int[] visited = new int[v];
+        ArrayList<Integer> bfs = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.offer(0);
+        
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            
+            if(visited[curr] != 1) {
+                visited[curr] = 1;
+                bfs.add(curr);
+                for(int i = 0; i < adj.get(curr).size(); i++) {
+                    q.offer(adj.get(curr).get(i));
+                }   
+            }
+        }
+        
+        return bfs;
+    }
+}
