527b5bf8403e86e89416b77ef40a401666260254
Anuj Thakur <82753410+anuj-thakur-513@users.noreply.github.com>
2023-07-14 13:27:03 +0530

    Added solution - LeetHub

---
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] grid = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    grid[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.nearest(grid);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find distance of nearest 1 in the grid for each cell.
+    public int[][] nearest(int[][] mat)
+    {
+        int m = mat.length, n = mat[0].length;
+        int[][] dist = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        // Initialize dist array and add all 1s to queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    dist[i][j] = 0;
+                    queue.offer(new int[]{i, j});
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        // BFS
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int i = cell[0], j = cell[1];
+            
+            for (int[] dir : dirs) {
+                int ni = i + dir[0];
+                int nj = j + dir[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                    if (dist[ni][nj] > dist[i][j] + 1) {
+                        dist[ni][nj] = dist[i][j] + 1;
+                        queue.offer(new int[]{ni, nj});
+                    }
+                }
+            }
+        }
+        
+        return dist;
+    }
+}
