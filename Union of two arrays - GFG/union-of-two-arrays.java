762c0287344a8d039ede199db6aa0aee0cf0adb6
Anuj Thakur <82753410+anuj-thakur-513@users.noreply.github.com>
2023-07-24 22:17:48 +0530

    Added solution - LeetHub

---
+++ Union of two arrays - GFG/union-of-two-arrays.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //Taking input using class Scanner
+		Scanner sc=new Scanner(System.in);
+		
+		//Taking total count of testcases
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    
+		    int n,m;
+		    
+		    //taking size of array a
+		    n=sc.nextInt();
+		    
+		    //taking size of array b
+		    m=sc.nextInt();
+		    
+		    //Creating 2 array of size n and m
+		    int a[]=new int[n];
+		    int b[]=new int[m];
+		    
+		    //inserting elements to array a
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    
+		    //inserting elements to array b
+		    for(int i=0;i<m;i++)
+		    {
+		        b[i]=sc.nextInt();
+		    }
+		    Solution ob=new Solution();
+		    //calling doUnion method and printing the results
+		    System.out.println(ob.doUnion(a,n,b,m));
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        HashSet<Integer> set = new HashSet<>();
+        for(int it: a) {
+            set.add(it);
+        }
+        for(int it: b) {
+            set.add(it);
+        }
+        
+        return set.size();
+    }
+}
